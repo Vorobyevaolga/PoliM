c627faa820fd7e7070cc975ef0a587a99a39835d
MI <Bocha555olga@yandex.ru>
2024-04-09 18:19:54 +0400

    Домашнее задание

---
+++ src/Bicycle.java
@@ -0,0 +1,18 @@
+public class Bicycle extends Transport {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем велосипед - " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку велосипеду- " + getModelName());
+    }
+}
